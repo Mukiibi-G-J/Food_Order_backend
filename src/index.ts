652e63de4c910efe03f4dee77c9f42c89a0f14fb
Mukiibi-G-J <mukiibijosephgilbert865@gmail.com>
2022-08-02 00:56:54 +0300

    part5 delopy on heroku

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { PORT } from './config';
 
 import dbConnection from './services/Database';
 import App from './services/ExpressApp';
@@ -9,8 +10,8 @@ const StartServer = async () => {
   await dbConnection();
 
   await App(app);
-  app.listen(8000, () => {
-    console.log('Server is running on port 8000');
+  app.listen(PORT, () => {
+    console.log(`Listening to port 8000 ${PORT}`);
   });
 };
 
--- src/services/ExpressApp.ts
+++ src/services/ExpressApp.ts
@@ -4,14 +4,13 @@ import {
   ShoppingRoute,
   VandorRoute,
 } from '../routes';
-import bodyParser from 'body-parser';
 import cors from 'cors';
 import path from 'path';
 import express, { Application } from 'express';
 
 export default async (app: Application) => {
-  app.use(bodyParser.json({ limit: '30mb' }));
-  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.json({ limit: '30mb' }));
+  app.use(express.urlencoded({ extended: true }));
   // const imagePath = path.join(__dirname, '../images');
 
   app.use('/images', express.static(path.join(__dirname, 'images')));
