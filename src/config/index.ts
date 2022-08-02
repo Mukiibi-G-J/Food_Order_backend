379a2e4d6e01caafde0e15ec960362efd978e10f
Mukiibi-G-J <mukiibijosephgilbert865@gmail.com>
2022-08-02 07:00:42 +0300

    part5 delopy on heroku & fixing Mongo-url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 // mongodb://localhost:27017/online_food_delivery
 export const MONGO_URI =
-  'mongodb+srv://admin:<admin>@cluster0.wmapd.mongodb.net/?retryWrites=true&w=majority';
+  'mongodb+srv://admin:admin@cluster0.wmapd.mongodb.net/?retryWrites=true&w=majority';
 
 export const PORT = process.env.PORT || 8000;
 
