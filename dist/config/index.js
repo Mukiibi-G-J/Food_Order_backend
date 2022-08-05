6435088feba5cec3d56b7ac5f917299b4738559a
Mukiibi-G-J <mukiibijosephgilbert865@gmail.com>
2022-08-05 20:50:13 +0300

    Hosting

--- dist/config/index.js
+++ dist/config/index.js
@@ -2,7 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.APP_SECRET = exports.PORT = exports.MONGO_URI = void 0;
 // mongodb://localhost:27017/online_food_delivery
+// export const MONGO_URI = 'mongodb://localhost:27017/online_food_delivery';
 exports.MONGO_URI = 'mongodb+srv://admin:admin@cluster0.wmapd.mongodb.net/?retryWrites=true&w=majority';
+// mongodb+srv://admin:@cluster0.wmapd.mongodb.net/?retryWrites=true&w=majority
 exports.PORT = process.env.PORT || 8000;
 exports.APP_SECRET = 'Our_APP_Secret';
 //# sourceMappingURL=index.js.map
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,8 @@
 // mongodb://localhost:27017/online_food_delivery
+// export const MONGO_URI = 'mongodb://localhost:27017/online_food_delivery';
 export const MONGO_URI =
   'mongodb+srv://admin:admin@cluster0.wmapd.mongodb.net/?retryWrites=true&w=majority';
-
+// mongodb+srv://admin:@cluster0.wmapd.mongodb.net/?retryWrites=true&w=majority
 export const PORT = process.env.PORT || 8000;
 
 export const APP_SECRET = 'Our_APP_Secret';
